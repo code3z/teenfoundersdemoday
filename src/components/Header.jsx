030a7c05552f9eb3c5958fb2335b308144a15ada
Ian <github@ianc.me>
2022-11-07 17:00:14 -0500

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,35 @@
+import { Button } from '@/components/Button'
+import { Container } from '@/components/Container'
+import { DiamondIcon } from '@/components/DiamondIcon'
+import Logo from '../images/tflogo.png'
+import Image from 'next/image'
+export function Header() {
+  return (
+    <header className="relative z-50 pb-11 lg:pt-11">
+      <Container className="flex flex-wrap items-center justify-center sm:justify-between lg:flex-nowrap">
+        <div className="mt-10 font-display text-xl font-bold text-blue-600 lg:mt-0 lg:grow lg:basis-0">
+          <Image
+            className="mr-3 inline drop-shadow-lg"
+            height={100}
+            src={Logo}
+          />
+        </div>
+        <div className="order-first -mx-4 flex flex-auto basis-full overflow-x-auto whitespace-nowrap border-b border-blue-600/10 py-4 font-mono text-sm text-blue-600 sm:-mx-6 lg:order-none lg:mx-0 lg:basis-auto lg:border-0 lg:py-0">
+          <div className="mx-auto flex items-center gap-4 px-4">
+            <p>
+              <time dateTime="2022-04-06">06 of April, 2022</time>
+            </p>
+            <DiamondIcon className="h-1.5 w-1.5 overflow-visible fill-current stroke-current" />
+            <p>Virtual</p>
+          </div>
+        </div>
+        <div className="hidden sm:mt-10 sm:flex lg:mt-0 lg:grow lg:basis-0 lg:justify-end">
+          <Button href="https://lu.ma/vcs">
+            Signup!
+            <span className="ml-2">🚀</span>
+          </Button>
+        </div>
+      </Container>
+    </header>
+  )
+}
---
+++ src/pages/index.jsx
@@ -0,0 +1,34 @@
+import Head from 'next/head'
+
+import { Footer } from '@/components/Footer'
+import { Header } from '@/components/Header'
+import { Hero } from '@/components/Hero'
+import { Newsletter } from '@/components/Newsletter'
+import { Schedule } from '@/components/Schedule'
+import { Speakers } from '@/components/Speakers'
+import { Sponsors } from '@/components/Sponsors'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>
+          Teen Founder Demo Day - see future Elon Musks present their startup
+        </title>
+        <meta
+          name="description"
+          content="Teen Founder Demo Day is a virtual event where incredible teen founders present their startup to VCs and the audience."
+        />
+      </Head>
+      <Header />
+      <main>
+        <Hero />
+        <Speakers />
+        {/* <Schedule />*/}
+        <Sponsors />
+        <Newsletter />
+      </main>
+      <Footer />
+    </>
+  )
+}
