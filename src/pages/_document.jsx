7b998fcf7df255d61f9df66cae5e1eb9c120c157
Ian <github@ianc.me>
2022-11-08 10:33:32 -0500

    volument

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -14,6 +14,11 @@ export default function Document() {
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=DM+Sans:wght@400;500;700&display=swap"
         />
+        <script
+          async
+          src="https://cdn.volument.com/beta/volument.js"
+          token="3715e2604b"
+        ></script>
       </Head>
       <body>
         <Main />
